dee5ad4bfc67a68a1a542c52aa3b6cd282cf194b
Francisco Manuel <fran319@correo.ugr.es>
2021-03-12 20:21:41 +0100

    local: menu y vista de posts añadida

--- picturegram/urls.py
+++ picturegram/urls.py
@@ -20,12 +20,16 @@ from django.urls import path
 
 from picturegram import views as local_views
 from posts import views as posts_views
+from users import views as users_views
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello-world/', local_views.hello_world),
-    path('time/', local_views.get_time),
-    path('sorted/', local_views.sorted_integer),
-    path('hi/<str:name>/<int:age>/', local_views.say_hi),
+    path('hello-world/', local_views.hello_world, name='hello_world'),
+    path('time/', local_views.get_time, name='time'),
+    path('sorted/', local_views.sorted_integer, name='sort'),
+    path('hi/<str:name>/<int:age>/', local_views.say_hi, name='hi'),
+    path('posts/', posts_views.list_posts, name='feed'),
+    path('users/login/', users_views.login_view, name='login')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
